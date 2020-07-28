7198c35e46133d6fca4e856450841554c4aa6d47
diquiroz <derek.quiroz@gmail.com>
2020-07-27 23:05:39 -0500

    added createPlayer() to clean up Play GameState     TODO: move attributes to class that can update as user chooses in-game options
    added spawnRockObstacles(float deltaTime)
        basically same as before, but PHYSICS!
        TODO: move attributes to classes for various obstacles - would allow for more versatile function, e.g. spawnObstacles(float deltaTime)

--- core/src/myers/test/states/Play.java
+++ core/src/myers/test/states/Play.java
@@ -4,36 +4,32 @@ import static myers.test.handlers.B2DVars.PPM;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import myers.test.MayhemGame;
+import myers.test.RockObstacle;
 import myers.test.handlers.GameStateManager;
 
 public class Play extends GameState{
 
+    // box2d stuff
     private World world;
     private Box2DDebugRenderer b2dr;
     private OrthographicCamera b2dCamera;
 
+    // spawn stuff
+    private float rockSpawnTimer = 0;
+    private float timeBetweenRockSpawns = 1f;
+
     public Play(GameStateManager gameStateManager) {
         super(gameStateManager);
 
+        // set up box2d stuff
         world = new World(new Vector2(0, 0), true);
         b2dr = new Box2DDebugRenderer();
 
-        // player
-        BodyDef bdef = new BodyDef();
-        bdef.position.set(144 / PPM, 128 / PPM);
-        bdef.type = BodyDef.BodyType.DynamicBody;
-        Body body = world.createBody(bdef);
-
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(8 / PPM,16 / PPM);
-        FixtureDef fdef = new FixtureDef();
-        fdef.shape = shape;
-        //fdef.restitution = 0 - 1;
-        body.createFixture(fdef);
+        // create player
+        createPlayer();
 
         // set up box2d camera
         b2dCamera = new OrthographicCamera();
@@ -48,6 +44,8 @@ public class Play extends GameState{
     @Override
     public void update(float deltaTime) {
         world.step(deltaTime, 6, 2);
+
+        spawnRockObstacles(deltaTime);
     }
 
     @Override
@@ -62,5 +60,42 @@ public class Play extends GameState{
     @Override
     public void dispose() {
 
+    }
+
+    private void createPlayer() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(144 / PPM, 128 / PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        Body body = world.createBody(bdef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(8 / PPM,16 / PPM);
+        FixtureDef fdef = new FixtureDef();
+        fdef.shape = shape;
+        fdef.restitution = 0.5f;
+        body.createFixture(fdef);
+        shape.dispose();
+
+
+    }
+
+    private void spawnRockObstacles(float deltaTime) {
+        rockSpawnTimer += deltaTime;
+        if (rockSpawnTimer > timeBetweenRockSpawns) {
+            BodyDef bdef = new BodyDef();
+            bdef.position.set((MayhemGame.random.nextFloat() * MayhemGame.VIRTUAL_WIDTH + 8) / PPM, (MayhemGame.VIRTUAL_HEIGHT + 8) / PPM);
+            bdef.type = BodyDef.BodyType.KinematicBody;
+            bdef.linearVelocity.set(0, -1.5f);
+            Body body = world.createBody(bdef);
+
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(8 / PPM,8 / PPM);
+            FixtureDef fdef = new FixtureDef();
+            fdef.shape = shape;
+            body.createFixture(fdef);
+            shape.dispose();
+
+            rockSpawnTimer -= timeBetweenRockSpawns;
+        }
     }
 }
