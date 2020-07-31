b6559febde6ced4c8624f6662319742b297c74e7
myersdennis94 <myers.dennis.94@gmail.com>
2020-07-30 22:09:13 -0500

    Adds LandObstacle object

---
+++ core/src/myers/test/entities/obstacles/LandObstacle.java
@@ -0,0 +1,34 @@
+package myers.test.entities.obstacles;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.*;
+
+import static myers.test.handlers.B2DVars.PPM;
+
+public class LandObstacle extends Obstacle {
+
+    private int sideLength;
+
+    public LandObstacle(World world) {
+        super(world);
+        sideLength = MathUtils.random(2,3);
+        spawnFreq = 3;
+        spawnPeriod = 0000000000;
+        spawnMax = 2;
+        velocityX = 0;
+        velocityY = -1.5f/sideLength;
+        shapeHX = 8*sideLength;
+        shapeHY = 8*sideLength;
+        switch (sideLength){
+            case 2:
+                name = "land22";
+                break;
+            case 3:
+                name = "land33";
+                break;
+            case 4:
+                name = "land44";
+                break;
+        }
+    }
+}
