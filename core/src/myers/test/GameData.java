dcf8d2b39f37933e4f72a691e1d2a7608ad23d04
myersdennis94 <myers.dennis.94@gmail.com>
2020-08-03 14:33:45 -0500

    Saves game info in gamedata

--- core/src/myers/test/GameData.java
+++ core/src/myers/test/GameData.java
@@ -3,7 +3,10 @@ package myers.test;
 public class GameData {
 
     private int highscore;
+    private int lastScore;
     private boolean musicOn;
+    private float lastTime;
+    private float bestTime;
 
     public int getHighscore() {
         return highscore;
@@ -12,6 +15,18 @@ public class GameData {
     public void setHighscore(int highscore) {
         this.highscore = highscore;
     }
+
+    public int getLastScore() { return lastScore; }
+
+    public void setLastScore(int lastScore) { this.lastScore = lastScore; }
+
+    public float getLastTime(){ return lastTime; }
+
+    public void setLastTime(float lastTime){ this.lastTime = lastTime; }
+
+    public float getBestTime(){ return bestTime; }
+
+    public void setBestTime(float bestTime){ this.bestTime = bestTime; }
 
     public boolean isMusicOn() {
         return musicOn;
@@ -23,5 +38,6 @@ public class GameData {
 
     public void printInfo(){
         System.out.printf("%2d. %-10s\n",highscore, musicOn);
+        System.out.printf("%06d %5.1f\n",lastScore,lastTime);
     }
 }
