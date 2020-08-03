d441e31385da1f54abf6911e0d3fe72ba7ef1d9e
myersdennis94 <myers.dennis.94@gmail.com>
2020-08-02 22:57:29 -0500

    Adds time to HUD

--- core/src/myers/test/states/Play.java
+++ core/src/myers/test/states/Play.java
@@ -16,7 +16,9 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.codeandweb.physicseditor.PhysicsShapeCache;
+import myers.test.GameData;
 import myers.test.MayhemGame;
 import myers.test.entities.Player;
 import myers.test.entities.obstacles.LandObstacle;
@@ -66,6 +68,10 @@ public class Play extends GameState{
     private boolean lockTop = true;
     private boolean lockSides = true;
 
+    // timing
+    private long startTime;
+
+
     public Play(GameStateManager gameStateManager) {
         super(gameStateManager);
 
@@ -97,6 +103,8 @@ public class Play extends GameState{
         b2dCamera.setToOrtho(false, MayhemGame.VIRTUAL_WIDTH / PPM, MayhemGame.VIRTUAL_HEIGHT / PPM);
 
         setUpBorders();
+
+        startTime = TimeUtils.millis();
     }
 
     @Override
@@ -231,10 +239,11 @@ public class Play extends GameState{
         spriteBatch.begin();
         //first row
         font.draw(spriteBatch, "Score", hudLeftX, hudRow1Y, hudSectionWidth, Align.left, false);
-        //font.draw(spriteBatch,"Time",hudCenterX,hudRow1Y,hudSectionWidth,Align.center,false);
+        font.draw(spriteBatch,"Time",hudCenterX,hudRow1Y,hudSectionWidth,Align.center,false);
 
         //second row
         font.draw(spriteBatch,String.format(Locale.getDefault(),"%06.0f",player.getScore()),hudLeftX,hudRow2Y,hudSectionWidth,Align.left,false);
+        font.draw(spriteBatch,String.format(Locale.getDefault(),"%5.1f",TimeUtils.timeSinceMillis(startTime)/1000f)+" s",hudCenterX,hudRow2Y,hudSectionWidth,Align.left,false);
 
         spriteBatch.end();
     }
@@ -257,8 +266,8 @@ public class Play extends GameState{
         // calculate hud margins
         hudVerticalMargin = font.getCapHeight()/2;
         hudLeftX = hudVerticalMargin;
-        hudRightX = MayhemGame.VIRTUAL_WIDTH*2/3 - hudLeftX;
-        hudCenterX = MayhemGame.VIRTUAL_WIDTH/3;
+        hudRightX = MayhemGame.VIRTUAL_WIDTH/3 - hudLeftX;
+        hudCenterX = MayhemGame.VIRTUAL_WIDTH*MayhemGame.SCALE/2+hudVerticalMargin;
         hudRow1Y = MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE - hudVerticalMargin;
         hudRow2Y = hudRow1Y - hudVerticalMargin - font.getCapHeight();
         hudSectionWidth = MayhemGame.VIRTUAL_WIDTH/3;
