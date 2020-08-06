72dd96453d07ed2dee6c5d4be12d4dfe6968dc40
myersdennis94 <myers.dennis.94@gmail.com>
2020-08-06 15:18:50 -0500

    Changes background rendering

--- core/src/group4/mayhembay/states/MainMenuScreen.java
+++ core/src/group4/mayhembay/states/MainMenuScreen.java
@@ -24,13 +24,6 @@ public class MainMenuScreen extends GameState {
     private final float TITLE_BUTTON_WIDTH = (float) (SCREEN_WIDTH / 2);
     private final int TITLE_BUTTON_HEIGHT = SCREEN_HEIGHT / 7;
 
-
-    // background
-    private TextureRegion[] backgrounds;
-    private float[] backgroundOffsets = {0,0,0,0};
-    private float backgroundMaxScrollingSpeed;
-
-
     /**
      * This constructor initializes the MainMenuScreen object
      *
@@ -49,14 +42,6 @@ public class MainMenuScreen extends GameState {
         selectionButtonActive = textureAtlas.findRegion("ship_selection_button05");
         selectionButtonInactive = textureAtlas.findRegion("ship_selection_button00");
         gameTitle = MayhemGame.textureAtlas.findRegion("game_title");
-
-        // background
-        backgrounds = new TextureRegion[4];
-        backgrounds[0] = textureAtlas.findRegion("tex_Water");
-        backgrounds[1] = textureAtlas.findRegion("water2");
-        backgrounds[2] = textureAtlas.findRegion("water3");
-        backgrounds[3] = textureAtlas.findRegion("water4");
-        backgroundMaxScrollingSpeed = (float)MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE / 4;
     }
 
     /**
@@ -116,7 +101,7 @@ public class MainMenuScreen extends GameState {
 
 //        Gdx.gl.glClearColor(255, 255 , 255, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        renderBackground(deltaTime);
+        MayhemGame.background.render(deltaTime);
 
         spriteBatch.begin();
 
@@ -178,29 +163,6 @@ public class MainMenuScreen extends GameState {
      */
     @Override
     public void dispose() {
-    }
-    /**
-     *  This methods is responsible for rendering the background on the main menu
-     * @param deltaTime a float that corresponds to the times passed since last update
-     */
-    private void renderBackground(float deltaTime){
-        spriteBatch.begin();
-
-        backgroundOffsets[0] += deltaTime * backgroundMaxScrollingSpeed / 8;
-        backgroundOffsets[1] += deltaTime * backgroundMaxScrollingSpeed / 4;
-        backgroundOffsets[2] += deltaTime * backgroundMaxScrollingSpeed / 2;
-        backgroundOffsets[3] += deltaTime * backgroundMaxScrollingSpeed;
-
-        for(int layer = 0; layer < backgroundOffsets.length; layer++){
-            if(backgroundOffsets[layer] > MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE){
-                backgroundOffsets[layer] = 0;
-            }
-            spriteBatch.draw(backgrounds[layer],0,-backgroundOffsets[layer],
-                    MayhemGame.VIRTUAL_WIDTH*MayhemGame.SCALE,MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE);
-            spriteBatch.draw(backgrounds[layer],0,-backgroundOffsets[layer]+
-                    MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE,MayhemGame.VIRTUAL_WIDTH*MayhemGame.SCALE,MayhemGame.VIRTUAL_HEIGHT*MayhemGame.SCALE);
-        }
-        spriteBatch.end();
     }
 }
 
